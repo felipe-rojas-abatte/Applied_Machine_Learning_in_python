53c2d06078804d51fa359158360161bd86b9f938
Felipe Rojas <felipe.rojas.abatte@gmail.com>
2020-07-13 21:47:51 -0400

    Assignment2 1st question done

--- 2nd_week/adspy_shared_utilities.py
+++ 2nd_week/adspy_shared_utilities.py
@@ -14,7 +14,7 @@ def load_crime_dataset():
     # Communities and Crime dataset for regression
     # https://archive.ics.uci.edu/ml/datasets/Communities+and+Crime+Unnormalized
 
-    crime = pd.read_table('readonly/CommViolPredUnnormalizedData.txt', sep=',', na_values='?')
+    crime = pd.read_table('/home/felipe/Documents/Programas/git_repository/Applied_Machine_Learning_in_python/2nd_week/CommViolPredUnnormalizedData.txt', sep=',', na_values='?')
     # remove features with poor coverage or lower relevance, and keep ViolentCrimesPerPop target column
     columns_to_keep = [5, 6] + list(range(11,26)) + list(range(32, 103)) + [145]  
     crime = crime.ix[:,columns_to_keep].dropna()
